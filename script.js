3baa9db6b734ec21687f105c6e26a104f79e59df
Arthur1asdf <arthurteng0819@gmail.com>
2024-06-27 21:17:29 -0400

    finished bocchi with bluging eyes

--- script.js
+++ script.js
@@ -38,3 +38,59 @@ document.addEventListener("mousemove", function (e) {
     pupil.style.transform = `translate(-50%, -50%) translate(${pupilX}px, ${pupilY}px)`;
   });
 });
+
+const hideBocchiOuterRadius = document.getElementById("hideBocchiOuterRadius");
+const pupil1 = document.getElementById("pupil1");
+const pupil2 = document.getElementById("pupil2");
+hideBocchiOuterRadius.addEventListener("mouseover", () => {
+  pupil1.style.backgroundImage = "url('/images/dotPupil.png')";
+  pupil2.style.backgroundImage = "url('/images/dotPupil.png')";
+  pupil1.style.backgroundSize = "900%";
+  pupil2.style.backgroundSize = "900%";
+  pupil1.style.backgroundPosition = "50% 50%";
+  pupil2.style.backgroundPosition = "50% 50%";
+});
+hideBocchiOuterRadius.addEventListener("mouseout", () => {
+  pupil1.style.backgroundImage = "url('/images/bocchi-eye.png')";
+  pupil2.style.backgroundImage = "url('/images/bocchi-eye2.png')";
+  pupil1.style.backgroundSize = "1200%";
+  pupil2.style.backgroundSize = "1200%";
+  pupil1.style.backgroundPosition = "45% 47%";
+  pupil2.style.backgroundPosition = "63% 52%";
+});
+
+const hideBocchi = document.getElementById("hideBocchi");
+const leftEye = document.getElementById("leftEye");
+const rightEye = document.getElementById("rightEye");
+const bocchi = document.getElementById("bocchi");
+const bulgingBocchi = document.getElementById("bulgingBocchi");
+let bocchiHidden = false;
+let bocchiPop;
+hideBocchi.addEventListener("click", () => {
+  clearTimeout(bocchiPop);
+  if (!bocchiHidden) {
+    bocchi.style.display = "none";
+    bocchiHidden = true;
+
+    leftEye.style.width = "180px";
+    rightEye.style.width = "160px";
+    leftEye.style.height = "110px";
+    rightEye.style.height = "100px";
+
+    leftEye.style.borderRadius = "40px 90px 90px 60px";
+    rightEye.style.borderRadius = "40px 80px 80px 60px";
+    bocchiPop = setTimeout(() => {
+      bulgingBocchi.style.display = "none";
+    }, 2000);
+  } else {
+    bocchiHidden = false;
+    bocchi.style.display = "block";
+    bulgingBocchi.style.display = "block";
+    leftEye.style.width = "90px";
+    rightEye.style.width = "80px";
+    leftEye.style.height = "90px";
+    rightEye.style.height = "80px";
+    leftEye.style.borderRadius = "50%";
+    rightEye.style.borderRadius = "50%";
+  }
+});
